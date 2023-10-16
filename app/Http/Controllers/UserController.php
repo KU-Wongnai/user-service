2be655e4fcf39f44ee8c3d1c78467200e5b4b7bd
Qu1etboy <nonzagreanthai@gmail.com>
2023-10-16 20:02:25 +0700

    feat: add regex to validate international phone number

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -202,7 +202,9 @@ class UserController extends Controller
      */
     public function createUserProfile(Request $request) {
         $validateInput = $request->validate([
-            'phone_number' => ['nullable', 'size:10'],
+            'phone_number' => ['nullable', 'regex:/\+(9[976]\d|8[987530]\d|6[987]\d|5[90]\d|42\d|3[875]\d|
+            2[98654321]\d|9[8543210]|8[6421]|6[6543210]|5[87654321]|
+            4[987654310]|3[9643210]|2[70]|7|1)\d{1,14}$/'], // regex for validate a phone number
             'birth_date' => ['nullable', 'date'],
             'address' => ['nullable', 'min:1', 'max:255'],
             'avatar' => ['nullable', 'string'],
